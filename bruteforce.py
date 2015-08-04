08318e224dc62960b281f25db33dec911be9da66
ariamgomez <ariamgomez@gmail.com>
2015-08-04 14:38:41 -0400

    Create bruteforce.py

---
+++ bruteforce.py
@@ -0,0 +1,19 @@
+## This square root calculator will depict the 'Exhaustive
+## Enumeration' a brute force approach.
+
+# Error
+epsilon = 0.01
+
+# Counters
+ans = 0.0
+count = 0.0
+
+x = raw_input ("Please enter a number, we will find its root ")
+x = float(x)
+
+
+while epsilon <= abs(x-(ans*ans)):
+    ans = ans + 0.001
+    count = count + 1
+print ("We tried this many times "), count
+print ("The root is"), ans
